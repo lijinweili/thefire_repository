05588f72dad6c4a8f494dab4e90ffa63be4b519c
Administrator <Administrator@tarena>
2019-06-28 09:18:30 +0800

    1.秒杀系统

---
+++ miaosha/src/main/java/com/miaosha/SpringbootHelloApplication.java
@@ -0,0 +1,25 @@
+package com.miaosha;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+//@Controller
+@MapperScan("com.miaosha.mapper")//为mapper接口创建代理对象
+public class SpringbootHelloApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello miaosha");
+		SpringApplication.run(SpringbootHelloApplication.class, args);
+	}
+
+/*	@RequestMapping("/")
+	@ResponseBody
+	 public String home() {
+		 return "lijinwei";
+	 }*/
+}
